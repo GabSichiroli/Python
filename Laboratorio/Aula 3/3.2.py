f94693b59d65c9a2cd788e6d6352c886364527b2
Sichiroli <sichiroli07@gmail.com>
2025-03-20 14:45:34 -0300

    p2

---
+++ Laboratorio/Aula 3/3.2.py
@@ -0,0 +1,7 @@
+base=int(input("\n\tInsira o valor da base:"))
+altura=int(input("\n\tInsira o valor da altura:"))
+
+if(base==altura):
+    print(f"\n\tQuadrado")
+else:
+    print(f"\n\tRetângulo")
