e61b27dc255bf00ba5d7e26cac90c1cc01b331ff
GabSichiroli <Sichiroli07@gmail.com>
2025-03-31 10:33:11 -0300

    aaa

---
+++ Projetos/Projeto 1/Aleatorizador.py
@@ -0,0 +1,5 @@
+import random
+
+# Gerar um número inteiro aleatório entre 1 e 10
+numero_aleatorio = random.randint(1, 10)
+print(f"Número aleatório gerado: {numero_aleatorio}")
---
+++ Projetos/Projeto 1/Algoritimo para separar os numeros.py
@@ -0,0 +1,11 @@
+#Separa o numero em suas respectivas casas
+
+base=9824
+
+b1=base//1000
+base=base-(b1*1000)
+b2=base//100
+base=base-(b2*100)
+b3=base//10
+b4=base%10
+
